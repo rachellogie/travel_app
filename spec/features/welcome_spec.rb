357addb162e6136d15ad1e01f818e564e7c5ee40
rachellogie <rachel.logie1@gmail.com>
2014-09-09 07:10:04 -0600

    User can see app name on homepage

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'homepage' do
+
+  scenario 'it says the app name on the homepage' do
+    visit root_path
+    expect(page).to have_content 'Travel App'
+  end
+end
