fc948a71be6adfa26e58944d88ea693bc3b38aae
rachellogie <rachel.logie1@gmail.com>
2014-09-09 07:29:29 -0600

    User can add a destination

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
 
   def index
-
+    @locations = Location.all
   end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -6,4 +6,15 @@ feature 'homepage' do
     visit root_path
     expect(page).to have_content 'Travel App'
   end
+
+  scenario 'user can add a desired destination' do
+    visit root_path
+    click_on 'Add desired destination'
+    fill_in 'Country', with: 'Cuba'
+    click_on 'Add'
+    within('.desired') do
+      expect(page).to have_content 'Cuba'
+    end
+  end
+
 end
